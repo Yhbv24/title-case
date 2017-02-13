a1f89de45b4a7575fb2021b4a7d42abd8e311181
Ash Laidlaw and Dan Lauby <ash@>
2017-02-13 10:02:46 -0800

    Add method to test multiple word capitalizations, test passes

--- test/TitleCaseGeneratorTest.php
+++ test/TitleCaseGeneratorTest.php
@@ -18,7 +18,15 @@
 
         function test_makeTitleCase_multipleWords()
         {
+            // Arrange
+            $test_TitleCaseGenerator = new TitleCaseGenerator;
+            $input = "the little mermaid";
 
+            // Act
+            $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+            // Assert
+            $this->assertEquals("The Little Mermaid", $result);
         }
     }
  ?>
